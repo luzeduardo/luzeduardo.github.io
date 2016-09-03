8ba4c365b3fe1e0aa9d8bd906e809b242402e807
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-03 17:48:09 -0300

    - defining alpine image fro local development - application shell architecture

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,9 +1,13 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('public/js/sw.js').then(function(reg) {
-    console.log('Yey!', reg);
-  }).catch(function(err) {
-    console.log('Boo!', err);
-  });
+  navigator.serviceWorker.register('public/js/sw.js').then(
+  	function(reg) {
+  		if(reg.installing){
+  			reg.installing.postMessage('Installing in page');
+  		}
+    	console.log('Yey!', reg);
+  	}).catch(function(err) {
+    	console.log('Boo!', err);
+  	});
 }
 // navigator.serviceWorker.ready.then(function(registrationNotificationTag){
 // 	registration.sync.register('outbox').then(function(){
