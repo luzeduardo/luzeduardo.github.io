3872845c1e7097342eb27874e8d0eed1ef9c760e
Eduardo Luz <luz.eduardo@gmail.com>
2017-04-15 13:44:09 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,53 +1,21 @@
 import React, { Component } from 'react';
-import './App.css';
-import Scanner from './Scanner';
-import Result from './Result';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import AppBar from 'material-ui/AppBar';
+import Layout from './Layout';
 
 class App extends Component {
-
-    constructor(props){
-        super(props)
-        this.state = {
-            scanning: false,
-            results: []
-        }
-    }
 
     render() {
         return (
             <div>
-
-                    <button onClick={this._scan.bind(this)}>{this.state.scanning ? 'Stop' : 'Start'}</button>
-                    <ul className="results">
-                        {this.state.results.map((result) => (<Result key={result.codeResult.code} result={result} />))}
-                    </ul>
-                    {this.state.scanning ? <Scanner onDetected={this._onDetected.bind(this)}/> : null}
-
+                <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                    <Layout />
+                </MuiThemeProvider>
             </div>
         );
     }
 
-    _scan() {
-        this.setState({scanning: !this.state.scanning});
-    }
-
-    _onDetected(result) {
-        this.setState({results: this.state.results.concat([result])});
-    }
-
-  // render() {
-  //   return (
-  //     <div className="App">
-  //       <div className="App-header">
-  //         <h2>Welcome</h2>
-  //       </div>
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
--- src/Layout.js
+++ src/Layout.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Scanner from './Scanner';
+import Result from './Result';
 
-const Layout = () => (
-    <RaisedButton label="Default" />
-);
+class Layout extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            scanning: false,
+            results: []
+        }
+    }
+
+    _scan() {
+        this.setState({scanning: !this.state.scanning});
+    }
+
+    _onDetected(result) {
+        this.setState({results: this.state.results.concat([result])});
+    }
+
+    render() {
+        return (
+            <div>
+                <RaisedButton label={this.state.scanning ? 'Parar leitura' : 'Ler código de barras'}
+                              primary={!this.state.scanning }
+                              secondary={this.state.scanning }
+                              onClick={this._scan.bind(this)} />
+
+                <ul className="results">
+                    {this.state.results.map((result) => (<Result key={result.codeResult.code} result={result} />))}
+                </ul>
+                {this.state.scanning ? <Scanner onDetected={this._onDetected.bind(this)}/> : null}
+
+            </div>
+        )
+    }
+}
 
 export default Layout;
