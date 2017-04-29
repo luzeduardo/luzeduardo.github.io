190f56e90c42d32b70eb85965cdcc3b5b125c4d8
Eduardo Luz <luz.eduardo@gmail.com>
2017-04-29 12:12:13 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppRouter from './AppRouter';
@@ -9,7 +9,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+                <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
                     <AppRouter />
                 </MuiThemeProvider>
             </div>
